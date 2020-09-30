61a4c4dfb0120f49fffdd8c5b2764c4450630e86
gulifeng <gulifeng@cn.ibm.com>
2020-09-30 21:40:30 +0800

    add activemq demo

---
+++ src/main/java/com/cloud/demo/activemq/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.cloud.demo.activemq.controller;
+
+import com.cloud.demo.activemq.common.ActiveManager;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.Destination;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping(value = "/test/producer", produces = "application/json")
+public class CustomerController {
+    /**
+     * 注入ActiveManager
+     */
+    @Autowired
+    ActiveManager activeManager;
+
+    /**
+     * 新增消息队列
+     */
+    @RequestMapping(value = "/add/queue", method = RequestMethod.GET)
+    public void addQueue() {
+        Destination destination = new ActiveMQQueue("yiliQueueTest");
+        //传入队列以及值
+        activeManager.send(destination, "success");
+    }
+
+    /**
+     * 新增消息队列
+     */
+    @RequestMapping(value = "/add/delay/queue", method = RequestMethod.GET)
+    public void addDelayQueue() {
+        System.out.println(LocalDateTime.now());
+        activeManager.delaySend("success", "yilipocDelayQueueTest", 5000L);
+    }
+}
---
+++ src/main/java/com/cloud/demo/activemq/controller/CustomerRest.java
@@ -0,0 +1,24 @@
+package com.cloud.demo.activemq.controller;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class CustomerRest {
+    /**
+     * 监听方
+     */
+    //监听注解
+    @JmsListener(destination = "yilipocQueueTest")
+    public void getQueue(String info) {
+        System.out.println("成功监听yilipocQueueTest消息队列，传来的值为:" + info);
+    }
+
+    //
+    @JmsListener(destination = "yilipocDelayQueueTest")
+    public void getDelayQueue(String info) {
+        System.out.println("成功监听yilipocDelayQueueTest消息队列，传来的值为:" + info + "当前时间为" + LocalDateTime.now());
+    }
+}
